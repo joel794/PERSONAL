d770530643175ed8587eb26596baa6bb047fd415
Joel Febin <72789113+joel794@users.noreply.github.com>
2024-09-12 20:26:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+//window scroll
+$(window).on("scroll", function(){
+    var scrollTop = $(window).scrollTop();
+    if(scrollTop >= 80){
+        $('body').addClass('fixed-header');
+    }else{
+        $('body').removeClass('fixed-header');
+    }
+});
+
+
